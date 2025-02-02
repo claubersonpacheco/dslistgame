d2435a5d3c6fba1f87f3425bb612f765ed2f9d7d
caubinho <caubinho@gmail.com>
2025-02-03 00:40:01 +0100

    Game by ID

--- src/main/java/com/superior/dslist/services/GameService.java
+++ src/main/java/com/superior/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.superior.dslist.dto.GameDTO;
 import com.superior.dslist.dto.GameMinDTO;
 import com.superior.dslist.entities.Game;
 import com.superior.dslist.repositories.GameRepository;
@@ -15,6 +17,15 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		
+		return new GameDTO(result);
+		
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		
 		List<Game> result = gameRepository.findAll();
